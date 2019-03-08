5d7fbc4272ca8ec94a47a02bea934bbf0eb0b511
Ralph Smith <rs2271@gmail.com>
2019-03-07 22:12:38 -0500

    update

--- index.php
+++ index.php
@@ -9,9 +9,6 @@ Whoops, it looks like you have an invalid PHP version.</h3></div><p>Magento supp
     exit;
 }
 
-/**
- * Compilation includes configuration file
- */
 define('MAGENTO_ROOT', getcwd());
 
 $compilerConfig = MAGENTO_ROOT . '/includes/config.php';
